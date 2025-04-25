b8d26a093070e002e0757de25bab079fa850a61c
Ayumu3746221 <a.kkt0307@gmail.com>
2025-04-25 23:19:49 +0900

    refactoring job-posting-controller

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -10,4 +10,8 @@ class JobPosting < ApplicationRecord
   validates :title, presence: true
 
   attribute :is_active, :boolean, default: true
+
+  scope :active, -> { where(is_active: true) }
+  scope :with_skill, ->(id) { joins(:job_posting_skills).where(job_posting_skills: { skill_id: id }).distinct }
+  scope :with_industry, ->(id) { joins(:job_posting_industries).where(job_posting_industries: { industry_id: id }).distinct }
 end
